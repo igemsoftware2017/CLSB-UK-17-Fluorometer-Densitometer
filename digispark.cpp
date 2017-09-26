e301e3e3d299d467ddf9191a13c29785066db8b1
Adam Jones <jones_adam@rocketmail.com>
2017-09-26 01:09:41 +0100

    Refactoring

--- digispark.cpp
+++ digispark.cpp
@@ -1,34 +1,16 @@
-// General TODOs:
-// TODO: replace all delays with DigiKeyboard.delay() - this ensures the device keeps sending stuff to computer saying it's alive otherwise will think it's disconnected
-// TODO: figure out what processing we can do on the device - can't just send interrupt stream but also have limited processing power & memory
-// TODO: ensuring pins 2 & 3 are ok for interrupts - especially 3 as it shares USB+ which could cause issues esp. with DigiKeyboard library
-// TODO: test viability of just putting the LEDs on 5V, cause may as well be always on
-
 #include "DigiKeyboard.h"
 
-// Set pins to power LEDs and get data from light to frequency converters
-// NB: pins 0 and 1 are selected for LEDs as onboard LED can interfere with data readings otherwise
-#define LED_F 0
-#define LED_OD 1
-#define LTF_F 2
-#define LTF_OD 3
+// Set pins to get data from light to frequency converters
+#define LED_5V 0
+#define LED_GND 1
+#define LTF 2
 
-// Counters of how many times the pin has changed. Useful as TSL235R outputs
+// Count how many times the pin has changed. Useful as TSL235R outputs
 // a square wave (i.e. changes), with frequency proportional to light intensity
-volatile unsigned long cntF = 0;
-unsigned long oldcntF = 0;
-unsigned long hz_F = 0;
-unsigned long timeSinceLastF;
-void irqF() {
-    cntF++;
-}
-
-volatile unsigned long cntOD = 0;
-unsigned long oldcntOD = 0;
-unsigned long hz_OD = 0;
-unsigned long timeSinceLastOD;
-void irqOD() {
-    cntOD++;
+volatile unsigned long cnt = 0;
+unsigned long timeSinceLast;
+void irq() {
+    cnt++;
 }
 
 void setup() {
@@ -37,47 +19,33 @@ void setup() {
     DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.delay(250);
 
-    // TODO: I changed the attachInterrupt lines to CHANGE - hopefully this works.
-    // We may need to account for double interrupts being called somewhere, however as
-    // OD is relative maybe not?
+    pinMode(LED_5V, OUTPUT);
+    pinMode(LED_GND, OUTPUT);
+    digitalWrite(LED_5V, HIGH);
+    digitalWrite(LED_GND, LOW);
 
-    pinMode(LTF_F, INPUT);
-    digitalWrite(LTF_F, HIGH);
-    attachInterrupt(LTF_F, irqF, CHANGE);
+    pinMode(LTF, INPUT);
+    digitalWrite(LTF, HIGH);
+    attachInterrupt(0, irq, CHANGE);
 
-    pinMode(LTF_OD, INPUT);
-    digitalWrite(LTF_OD, HIGH);
-    attachInterrupt(LTF_OD, irqOD, CHANGE);
-
-    DigiKeyboard.delay(250);
-}
-
-bool getMeasurement(volatile unsigned long *cnt, unsigned long *oldcnt, unsigned long *hz, unsigned long* timeSinceLast) {
-    // Check not already measuring
-    if (millis() - *timeSinceLast < 1000)
-        return false;
-
-    // Reset timer
-    *timeSinceLast = millis();
-
-    // Save # counts in last second i.e. the frequency
-    *hz = rand() % 20000;//*cnt - *oldcnt;
-
-    // Reset these to zero to prevent buffer overflow
-    // TODO: Is this really necessary? Quick estimate gives this will only happen after 5 days.
-    *cnt = 0;
-    *oldcnt = 0;
-
-    return true;
+    // Reset timer and count to zero so first result is not overly high
+    // as will have been counting for brief setup period
+    cnt = 0;
+    timeSinceLast = millis();
 }
 
 void loop() {
-    if(getMeasurement(&cntF, &oldcntF, &hz_F, &timeSinceLastF) != -1) {
-        DigiKeyboard.print('F' + String(hz_F) + ',');
-    };
-    if(getMeasurement(&cntOD, &oldcntOD, &hz_OD, &timeSinceLastOD) != -1) {
-        DigiKeyboard.print("OD" + String(hz_OD) + ',');
-    };
+    // Check not already measuring
+    if (millis() - timeSinceLast > 1000) {
+        // Print # counts in last second i.e. the frequency
+        DigiKeyboard.print(String(cnt) + ',');
 
-    DigiKeyboard.delay(400);
+        // Reset to zero for next run
+        cnt = 0;
+
+        // Reset timer
+        timeSinceLast = millis();
+    }
+
+    DigiKeyboard.delay(20);
 }
